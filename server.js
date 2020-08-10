44f31b9f9a3f037acc944b63cb1beee49ca1e97a
Dai Vuong <vxuandai@gmail.com>
2020-08-11 00:03:33 +0700

    add cors

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express')
+var cors = require('cors')
 var { graphqlHTTP } = require('express-graphql')
 var { buildSchema } = require('graphql')
 
@@ -70,6 +71,8 @@ var root = {
 }
 
 var app = express()
+
+app.use(cors())
 
 app.use('/graphql', graphqlHTTP({
     schema: schema,
